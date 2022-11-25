232ed7952a15b4234994cc687de744cf35fbd376
lite <te.li1@pactera.com>
2022-11-25 18:35:25 +0800

    init

---
+++ src/main/java/com/example/demo/controller/AController.java
@@ -0,0 +1,29 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class AController {
+	@GetMapping("/classloader")
+	public String classloaderTest() {
+		String path01 = "default";
+		String path02 = "default";
+
+		try {
+			path01 = ClassLoader.getSystemClassLoader().getResource("temp/a.txt").getPath();
+		}catch(Exception e) {
+			System.out.println("ClassLoader wrong");
+		}
+		
+		try {
+			path02 = AController.class.getClassLoader().getResource("temp/a.txt").getPath();
+		}catch(Exception e) {
+			System.out.println("ACon class");
+		}
+
+		return "1:"+path01+"; 2:"+path02;
+	}
+}
